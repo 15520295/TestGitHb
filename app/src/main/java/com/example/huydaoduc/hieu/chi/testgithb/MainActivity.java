f9c1cc84ab6e5e3814a70a1f629e3e4f57fddc99
HuyDaoDuc <15520295>
2018-03-14 20:30:08 +0700

    Chi

--- app/src/main/java/com/example/huydaoduc/hieu/chi/testgithb/MainActivity.java
+++ app/src/main/java/com/example/huydaoduc/hieu/chi/testgithb/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         
         huy huy = new huy();
+        Chi chi = new Chi();
     }
 }
