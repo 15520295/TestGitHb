2caa4577d4c7e9a4b39afb800fe2c371eac6c100
HuyDaoDuc <15520295>
2018-03-14 15:00:22 +0700

    huy

---
+++ app/src/main/java/com/example/huydaoduc/hieu/chi/testgithb/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.huydaoduc.hieu.chi.testgithb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        huy huy = new huy();
+    }
+}
---
+++ app/src/main/java/com/example/huydaoduc/hieu/chi/testgithb/huy.java
@@ -0,0 +1,9 @@
+package com.example.huydaoduc.hieu.chi.testgithb;
+
+/**
+ * Created by HuyDaoDuc on 14/03/2018.
+ */
+
+public class huy {
+    int huy;
+}
